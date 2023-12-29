8444ff94b303372bb10060fed3c6e25ef69efa0c
lucaslingle <lucasdaxlingle@gmail.com>
2023-12-29 00:00:06 -0800

    prelminary commit

--- scripts/configs/config_tiny.py
+++ scripts/configs/config_tiny.py
@@ -6,16 +6,19 @@ def get_config():
     config = config_dict.ConfigDict()
     config.model_size = "tiny"
 
+    # basics
     config.param_dtype = jnp.float32
     config.dtype = jnp.bfloat16
     config.sow_intermediates = False
 
-    config.hftr_tokenizer_name = "GPT2TokenizerFast"  # huggingface transformers tokeniz
-    config.hftr_tokenizer_shortname = "gpt2"  # huggingface transformers tokeniz
-    config.hfds_identifier = "skylion007/openwebtext"  # huggingface dataset identifier
-    config.hfds_config = None  # huggingface dataset config
-    config.hfds_datacol = "text"  # huggingface dataset data column
+    # huggingface tokenizer and dataset settings
+    config.hftr_tokenizer_name = "GPT2TokenizerFast"
+    config.hftr_tokenizer_shortname = "gpt2"
+    config.hfds_identifier = "skylion007/openwebtext"
+    config.hfds_config = None
+    config.hfds_datacol = "text"
 
+    # batch size, sequence length, architecture settings
     config.tokens_per_global_batch = 65536
     config.sequence_len = 512
     config.d_model = 128
@@ -26,12 +29,15 @@ def get_config():
     config.act_name = "relu"  # any activation defined jax.nn
     config.act_square = False  # activation squaring
 
+    # optimization settings
     config.grad_clip = 1.0  # gradient clip, applied globally using all parameter grads
     config.lr_max = 0.04  # maximum main lr; scaled by mu-parameterization and schedule
     config.adam_b1 = 0.9
     config.adam_b2 = 0.98
     config.adam_eps = 1e-9
     config.wd_lam = 0.01  # weight decay coefficient, is multiplied by each param's lr
+
+    # periodic action settings
     config.n_print_step = 10  # print every
     config.n_save_step = 5_000  # checkpoint every
     config.n_eval_step = 100  # eval steps per checkpoint
