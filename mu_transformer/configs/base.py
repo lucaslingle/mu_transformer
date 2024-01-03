87f0db30afef7b40d90198c3d1da439f1541d54c
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-03 00:43:48 -0800

    commit

--- mu_transformer/configs/base.py
+++ mu_transformer/configs/base.py
@@ -33,7 +33,7 @@ def get_base_config():
 
     # architecture
     config.n_layer = 24
-    config.d_head = 32
+    config.d_head = 128
     config.ff_multiple = 4
     config.rotary_base = 10_000
     config.act_name = "relu"  # any activation defined jax.nn
