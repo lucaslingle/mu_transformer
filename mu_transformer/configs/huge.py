4620db96b3d9f92a6bb497f601eb3a5c4417b048
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-03 07:00:46 -0800

    notes

--- mu_transformer/configs/huge.py
+++ mu_transformer/configs/huge.py
@@ -19,8 +19,8 @@ def get_config():
     config.model_size = "huge"
 
     # mesh
-    config.n_mesh_rows = 1
-    config.n_mesh_cols = 16
+    config.n_mesh_rows = 4
+    config.n_mesh_cols = 4
     config.n_mesh_planes = 8
 
     # width
