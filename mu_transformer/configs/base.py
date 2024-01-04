66ea70849b5d21a4eb74a932911ec5875d02d667
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-03 20:29:03 -0800

    use shazeer 3d parallelism again

--- mu_transformer/configs/base.py
+++ mu_transformer/configs/base.py
@@ -32,7 +32,7 @@ def get_base_config():
     # architecture
     config.param_dtype = jnp.float32
     config.dtype = jnp.bfloat16
-    config.output_logits_dtype = jnp.float32
+    config.output_logits_dtype = jnp.bfloat16
     config.n_layer = 12
     config.d_head = 256
     config.ff_multiple = 4
