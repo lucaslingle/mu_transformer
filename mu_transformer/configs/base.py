0582fe592a82569e85329ba18ba941204814b1ab
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-07 18:51:36 -0800

    commit

--- mu_transformer/configs/base.py
+++ mu_transformer/configs/base.py
@@ -27,7 +27,7 @@ def get_base_config():
     config.hfds_identifier = "skylion007/openwebtext"
     config.hfds_config = None
     config.hfds_datacol = "text"
-    config.hfds_stream_data = True  # be careful: if False, a large download may occur!!
+    config.hfds_stream_data = True
     config.sequence_len = 512
 
     # architecture
