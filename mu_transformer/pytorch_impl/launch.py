03c05333f3dd81c60c5ae1b15e56188d59e8e99c
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-24 00:50:48 -0800

    temp changes for colab

--- mu_transformer/pytorch_impl/launch.py
+++ mu_transformer/pytorch_impl/launch.py
@@ -19,6 +19,7 @@ import sys
 import time
 from collections import namedtuple
 
+import numpy as np
 import torch
 import torch.cuda as cuda
 import torch.nn as nn
@@ -287,19 +288,19 @@ def train_loop():
     start_step = 0  # todo: parse from checkpoint name
 
     batch_size = global_batch_size_factory()  # // dist.get_world_size()  # todo
-    dataset_shard = get_dataset(
-        hfds_identifier=FLAGS.config.hfds_identifier,
-        hfds_config=FLAGS.config.hfds_config,
-        hfds_datacol=FLAGS.config.hfds_datacol,
-        hfds_buffer_size=FLAGS.config.hfds_buffer_size,
-        hftr_tokenizer=tokenizer_factory(),
-        split_name="train",
-        batch_size=batch_size,
-        sequence_len=FLAGS.config.sequence_len,
-        pcount=1,  # dist.get_world_size(),  # todo
-        pindex=0,  # dist.get_rank(),  # todo
-        workdir=FLAGS.workdir,
-    )
+    # dataset_shard = get_dataset(
+    #     hfds_identifier=FLAGS.config.hfds_identifier,
+    #     hfds_config=FLAGS.config.hfds_config,
+    #     hfds_datacol=FLAGS.config.hfds_datacol,
+    #     hfds_buffer_size=FLAGS.config.hfds_buffer_size,
+    #     hftr_tokenizer=tokenizer_factory(),
+    #     split_name="train",
+    #     batch_size=batch_size,
+    #     sequence_len=FLAGS.config.sequence_len,
+    #     pcount=1,  # dist.get_world_size(),  # todo
+    #     pindex=0,  # dist.get_rank(),  # todo
+    #     workdir=FLAGS.workdir,
+    # )
 
     logging.info("Starting training loop...")
     best_val_loss = float("inf")
@@ -309,23 +310,23 @@ def train_loop():
     n_total_step = FLAGS.config.n_pretrain_step + FLAGS.config.n_finetune_step
     for step in range(start_step, n_total_step + 1):
         # run a training step
-        batch = get_batch(
-            dataset_shard,
-            batch_size=batch_size,
-            sequence_len=FLAGS.config.sequence_len,
-            step=step,
-            out_dtype=np.int64,
-        )
+        # batch = get_batch(
+        #     dataset_shard,
+        #     batch_size=batch_size,
+        #     sequence_len=FLAGS.config.sequence_len,
+        #     step=step,
+        #     out_dtype=np.int64,
+        # )
         state, metrics = train_step(
             state=state,
-            # batch=torch.remainder(
-            #     input=torch.arange(
-            #         end=batch_size * FLAGS.config.sequence_len,
-            #         device=state.model.hps.device,
-            #     ).view(batch_size, -1),
-            #     other=state.model.hps.n_vocab,
-            # ),
-            batch=torch.from_numpy(batch).to(state.model.hps.device),
+            batch=torch.remainder(
+                input=torch.arange(
+                    end=batch_size * FLAGS.config.sequence_len,
+                    device=state.model.hps.device,
+                ).view(batch_size, -1),
+                other=state.model.hps.n_vocab,
+            ),
+            # batch=torch.from_numpy(batch).to(state.model.hps.device),
         )
         # occasionally print metrics
         if step % FLAGS.config.n_print_step == 0:
--- mu_transformer/pytorch_impl/model.py
+++ mu_transformer/pytorch_impl/model.py
@@ -69,33 +69,23 @@ class RMSNorm(nn.Module):
         return x / rms[..., None]
 
 
-def sinusoidal_embs(length, width, base, dtype, device):
-    # B, T, H, D
-    positions = torch.arange(length, device=device)
-    dimensions = torch.arange(width // 2, device=device)
-    ang_freqs = torch.pow(base, -dimensions / (width // 2))
-
-    # expand to a shape broadcastable with q/k dims
-    positions = torch.reshape(positions, [1, length, 1, 1])
-    ang_freqs = torch.reshape(ang_freqs, [1, 1, 1, width // 2])
-
-    radians = positions * ang_freqs
-    cos = torch.cos(radians).to(dtype)
-    sin = torch.sin(radians).to(dtype)
-    return cos, sin
-
-
 class RotaryEncoding(nn.Module):
     def __init__(self, hps: TransformerConfig) -> None:
         super().__init__()
         self.hps = hps
-        cos, sin = sinusoidal_embs(
-            length=self.hps.sequence_len,
-            width=self.hps.d_head,
-            base=self.hps.rotary_base,
-            dtype=self.hps.dtype,
-            device=self.hps.device,
-        )
+
+        positions = torch.arange(self.hps.sequence_len, device=self.hps.device)
+        channels = torch.arange(self.hps.d_head // 2, device=self.hps.device)
+        ang_freqs = torch.pow(self.hps.rotary_base, -channels / (self.hps.d_head // 2))
+
+        # expand to a shape broadcastable with q/k dims
+        positions = torch.reshape(positions, [1, self.hps.sequence_len, 1, 1])
+        ang_freqs = torch.reshape(ang_freqs, [1, 1, 1, self.d_head // 2])
+
+        radians = positions * ang_freqs
+        cos = torch.cos(radians).to(self.hps.dtype)
+        sin = torch.sin(radians).to(self.hps.dtype)
+
         self.register_buffer("cos", cos)
         self.register_buffer("sin", sin)
 
@@ -157,28 +147,28 @@ class MultiheadSelfAttention(nn.Module):
         )
         self.rope = RotaryEncoding(self.hps)
 
-    def forward(self, x, intermediates, layer_id):
-        intermediates.coord_check_l1("ax_l1", x, layer_id)
+    def forward(self, x):  # , intermediates, layer_id):
+        # intermediates.coord_check_l1("ax_l1", x, layer_id)
 
         q = torch.einsum("btm,mhd->bthd", x, self.w_aq.to(self.hps.dtype))
         k = torch.einsum("btm,mhd->bthd", x, self.w_ak.to(self.hps.dtype))
         v = torch.einsum("btm,mhd->bthd", x, self.w_av.to(self.hps.dtype))
-        intermediates.coord_check_l1("aq_l1", q, layer_id)
-        intermediates.coord_check_l1("ak_l1", k, layer_id)
-        intermediates.coord_check_l1("av_l1", v, layer_id)
+        # intermediates.coord_check_l1("aq_l1", q, layer_id)
+        # intermediates.coord_check_l1("ak_l1", k, layer_id)
+        # intermediates.coord_check_l1("av_l1", v, layer_id)
 
         if self.hps.rotary_base > 0:
             q = self.rope(q)
             k = self.rope(k)
-            intermediates.coord_check_l1("aqr_l1", q, layer_id)
-            intermediates.coord_check_l1("akr_l1", k, layer_id)
+            # intermediates.coord_check_l1("aqr_l1", q, layer_id)
+            # intermediates.coord_check_l1("akr_l1", k, layer_id)
 
         # current flash impl doesnt allow storing intermediates like the avg qk scale
         # o = flash_attn_func(q, k, v, softmax_scale=1.0 / self.hps.d_head, causal=True)
 
         s = torch.einsum("bihd,bjhd->bhij", q, k)
         s = s / self.hps.d_head
-        intermediates.coord_check_l1("as_l1", s, layer_id)
+        # intermediates.coord_check_l1("as_l1", s, layer_id)
 
         i = torch.arange(self.hps.sequence_len, device=self.hps.device)[..., None]
         j = torch.arange(self.hps.sequence_len, device=self.hps.device)[None, ...]
@@ -187,14 +177,14 @@ class MultiheadSelfAttention(nn.Module):
         s = s - torch.tensor([INF_APPROX], dtype=x.dtype, device=self.hps.device) * mask
 
         p = F.softmax(s, dim=-1)
-        intermediates.coord_check_l1("ap_l1", p, layer_id)
+        # intermediates.coord_check_l1("ap_l1", p, layer_id)
 
         o = torch.einsum("bhij,bjhd->bihd", p, v)
-        intermediates.coord_check_l1("ao_l1", o, layer_id)
+        # intermediates.coord_check_l1("ao_l1", o, layer_id)
 
         r = torch.einsum("bihd,hdm->bim", o, self.w_ao.to(self.hps.dtype))
-        intermediates.coord_check_l1("ar_l1", r, layer_id)
-        return r, intermediates
+        # intermediates.coord_check_l1("ar_l1", r, layer_id)
+        return r  # , intermediates
 
 
 class MultiLayerPerceptron(nn.Module):
@@ -225,20 +215,20 @@ class MultiLayerPerceptron(nn.Module):
             ),
         )
 
-    def forward(self, x, intermediates, layer_id):
-        intermediates.coord_check_l1("fx_l1", x, layer_id)
+    def forward(self, x):  # , intermediates, layer_id):
+        # intermediates.coord_check_l1("fx_l1", x, layer_id)
 
         x = torch.einsum("btm,mf->btf", x, self.w_fi.to(self.hps.dtype))
-        intermediates.coord_check_l1("fp_l1", x, layer_id)
+        # intermediates.coord_check_l1("fp_l1", x, layer_id)
 
         x = getattr(F, self.hps.act_name)(x)
         if self.hps.act_square:
             x = torch.pow(x, 2)
-        intermediates.coord_check_l1("fa_l1", x, layer_id)
+        # intermediates.coord_check_l1("fa_l1", x, layer_id)
 
         x = torch.einsum("btf,fm->btm", x, self.w_fo.to(self.hps.dtype))
-        intermediates.coord_check_l1("fr_l1", x, layer_id)
-        return x, intermediates
+        # intermediates.coord_check_l1("fr_l1", x, layer_id)
+        return x  # , intermediates
 
 
 class TransformerBlock(nn.Module):
@@ -250,15 +240,22 @@ class TransformerBlock(nn.Module):
         self.norm2 = RMSNorm(self.hps)
         self.mlp = MultiLayerPerceptron(self.hps)
 
-    def _forward(self, x, intermediates, layer_id):
-        r1, intermediates = self.mha(self.norm1(x), intermediates, layer_id)
+    def _forward(self, x):  # , intermediates, layer_id):
+        # r1, intermediates = self.mha(self.norm1(x), intermediates, layer_id)
+        # x = x + r1
+        # r2, intermediates = self.mlp(self.norm2(x), intermediates, layer_id)
+        # x = x + r2
+        # return x
+
+        r1 = self.mha(self.norm1(x))
         x = x + r1
-        r2, intermediates = self.mlp(self.norm2(x), intermediates, layer_id)
+        r2 = self.mlp(self.norm2(x))
         x = x + r2
         return x
 
-    def forward(self, x, intermediates, layer_id):
-        return remat(self._forward, x, intermediates, layer_id, use_reentrant=True)
+    def forward(self, x):  # , intermediates, layer_id):
+        # return remat(self._forward, x, intermediates, layer_id, use_reentrant=True)
+        return remat(self._forward, x, use_reentrant=True)
 
 
 class Embedding(nn.Module):
@@ -295,10 +292,11 @@ class PredictionHead(nn.Module):
 
     def forward(self, x):
         x = self.norm(x)
-        if self.training:
-            output_logits_dtype = self.hps.output_logits_dtype
-        else:
-            output_logits_dtype = self.hps.param_dtype
+        output_logits_dtype = self.hps.output_logits_dtype
+        # if self.training:
+        #     output_logits_dtype = self.hps.output_logits_dtype
+        # else:
+        #     output_logits_dtype = self.hps.param_dtype
         x = torch.einsum("btm,mv->btv", x, self.w_eo.to(output_logits_dtype))
         return x
 
@@ -314,13 +312,16 @@ class Transformer(nn.Module):
         self.predict = PredictionHead(self.hps)
 
     def forward(self, x):
-        intermediates = Intermediates(enabled=self.hps.sow_intermediates)
+        # intermediates = Intermediates(enabled=self.hps.sow_intermediates)
         x = F.pad(x[:, 0:-1], (1, 0), value=self.hps.bos_token_id)
         x = self.embed(x)
+        # for layer_id in range(self.hps.n_layer):
+        #     x, intermediates = self.stack[layer_id](x, intermediates, layer_id)
         for layer_id in range(self.hps.n_layer):
-            x, intermediates = self.stack[layer_id](x, intermediates, layer_id)
+            x = self.stack[layer_id](x)
         x = self.predict(x)
-        return dict(
-            logprobs=F.log_softmax(x, dim=-1),
-            intermediates=intermediates,
-        )
+        # return dict(
+        #     logprobs=F.log_softmax(x, dim=-1),
+        #     intermediates=intermediates,
+        # )
+        return F.log_softmax(x, dim=-1)
