0159c062862314753516f42fdf31a302976f03a8
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-28 19:42:11 -0800

    support sp, sp++, mup, and set config for scaling experiments

--- mu_transformer/jax_impl/model.py
+++ mu_transformer/jax_impl/model.py
@@ -305,7 +305,7 @@ class Embedding(nn.Module):
         e_init = init.normal(1.0)  # appendix b.1
         w_emb = self.param(
             "w_ei",
-            nn.with_partitioning(e_init, MESH_AXES["NC"], self.global_mesh),
+            nn.with_partitioning(e_init, MESH_AXES["NN"], self.global_mesh),
             [self.hps.n_vocab, self.hps.d_model],
             self.hps.param_dtype,
         )
