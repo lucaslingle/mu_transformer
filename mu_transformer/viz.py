ea34df7b5d0cf4d394b1f71850449c478fd29f5e
lucaslingle <lucasdaxlingle@gmail.com>
2024-02-07 19:52:26 -0800

    commit

--- mu_transformer/viz.py
+++ mu_transformer/viz.py
@@ -1,17 +1,19 @@
+import math
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
 sns.set_theme(style="darkgrid")
 
-df = pd.read_csv("temp.csv")
-df["log_lr_base"] = df["lr_base"].map(lambda y: int(y.split("**")[-1].split(" ")[-1]))
+df = pd.read_csv("temp_dev19.csv", sep="	", header=0)
+df["log2_LR"] = df["LR"].map(lambda y: math.log2(y))
 print(df.head())
 
 # Plot the responses for different events and regions
-g = sns.FacetGrid(df, col="Parameterization", hue="d_model", palette="viridis")
+g = sns.FacetGrid(df, col="Rule", hue="Width", palette="viridis")
 g.set_titles("{col_name}")
-g.map(sns.lineplot, "log_lr_base", "Val loss final", legend="brief")
+g.map(sns.lineplot, "log2_LR", "Loss", legend="brief")
 g.add_legend(title="width")
 g.set_xlabels("log2(lr)")
 g.set_ylabels("val loss")
