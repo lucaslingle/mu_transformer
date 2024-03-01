bc2152d767673a32b886f45d124a9fad5814d44d
lucaslingle <lucasdaxlingle@gmail.com>
2024-02-29 22:58:14 -0800

    commit

--- mu_transformer/jax_impl/launch.py
+++ mu_transformer/jax_impl/launch.py
@@ -819,7 +819,7 @@ def main(argv):
     logging.info("Creating W&B connection...")
     if jax.process_index() == 0:
         wandb.init(
-            project="mu_transformer_paper_improved",
+            project="mu_transformer_paper_zero",
             group=FLAGS.experiment_group,
             config=vars(FLAGS.config)["_fields"],
             resume="never" if FLAGS.wb_run is None else "must",
